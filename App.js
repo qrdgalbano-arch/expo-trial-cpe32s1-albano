60808f0b96fc0439ad67aa70f0695296b593fe0e
Reagan Dantinaise G. Albano <qrdgalbano@tip.edu.ph>
2026-03-27 15:16:39 +0800

    March 27, 2026

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.Titletext}>My Awesome Project</Text>
+      <Text style = {styles.Bodytext}>By: Reagan Dantinaise G. Albano</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Titletext: {
+    fontFamily: 'serif',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+    Bodytext: {
+    fontFamily: 'serif',
+    fontSize: 14,
   },
 });
