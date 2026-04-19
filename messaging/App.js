e54a045fb56ef07ac73cf86946687a4799098493
Reagan Dantinaise G. Albano <qrdgalbano@tip.edu.ph>
2026-04-20 02:28:07 +0800

    Hands-on Activity 12.2 (Supplementary Activity Version), April 20, 2026

--- messaging/App.js
+++ messaging/App.js
@@ -1,16 +1,121 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  Alert,
+  BackHandler,
+  Image,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 import Status from './components/Status';
+import MessageList from './components/MessageList';
+import {
+  createImageMessage,
+  createLocationMessage,
+  createTextMessage,
+} from './utils/MessageUtils';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Status />
-      <View style={styles.content} />
-      <View style={styles.inputMethodEditor} />
-      <View style={styles.toolbar} />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    messages: [
+      createImageMessage('https://unsplash.it/300/300'),
+      createTextMessage('World'),
+      createTextMessage('Hello'),
+      createLocationMessage({
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }),
+    ],
+    fullscreenImageId: null,
+  };
+
+  componentDidMount() {
+    this.subscription = BackHandler.addEventListener('hardwareBackPress', () => {
+      const { fullscreenImageId } = this.state;
+      if (fullscreenImageId) {
+        this.dismissFullscreenImage();
+        return true;
+      }
+      return false;
+    });
+  }
+
+  componentWillUnmount() {
+    this.subscription.remove();
+  }
+
+  dismissFullscreenImage = () => {
+    this.setState({ fullscreenImageId: null });
+  };
+
+  handlePressMessage = ({ id, type }) => {
+    switch (type) {
+      case 'text':
+        Alert.alert(
+          'Delete message?',
+          null,
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+              text: 'Delete',
+              style: 'destructive',
+              onPress: () => {
+                const { messages } = this.state;
+                this.setState({
+                  messages: messages.filter(message => message.id !== id),
+                });
+              },
+            },
+          ],
+        );
+        break;
+      case 'image':
+        this.setState({ fullscreenImageId: id });
+        break;
+      default:
+        break;
+    }
+  };
+
+  renderFullscreenImage = () => {
+    const { messages, fullscreenImageId } = this.state;
+    if (!fullscreenImageId) return null;
+    const image = messages.find(message => message.id === fullscreenImageId);
+    if (!image) return null;
+    const { uri } = image;
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={this.dismissFullscreenImage}
+      >
+        <Image style={styles.fullscreenImage} source={{ uri }} />
+      </TouchableHighlight>
+    );
+  };
+
+  renderMessageList() {
+    const { messages } = this.state;
+    return (
+      <View style={styles.content}>
+        <MessageList messages={messages} onPressMessage={this.handlePressMessage} />
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Status />
+        {this.renderMessageList()}
+        <View style={styles.inputMethodEditor} />
+        <View style={styles.toolbar} />
+        {this.renderFullscreenImage()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -30,5 +135,14 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: 'rgba(0,0,0,0.04)',
     backgroundColor: 'white',
+  },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 100,
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: 'contain',
   },
 });
